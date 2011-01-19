f8c81415853dd63cabadb7e2fe4885066473614a
Your Name <ayalgelles@gmail.com>
2011-01-19 02:23:47 +0200

    blaa

--- public/fbconnect.js
+++ public/fbconnect.js
@@ -42,23 +42,6 @@ function nav(url) {
 var fbuid = null;
 
 
-window.fbAsyncInit = function() {
-    
-    FB.init({
-		appId  : '140153199345253',
-		status : true, // check login status
-		cookie : true, // enable cookies to allow the server to access the session
-		xfbml  : true  // parse XFBML
-	    });
-    };
-
-(function() {
-     var e = document.createElement('script');
-     e.async = true;
-     e.src = document.location.protocol +
-	 '//connect.facebook.net/en_US/all.js';
-     document.getElementById('fb-root').appendChild(e);
- }());
 
 
 //FB.init(API_KEY, siteUrl + "xd_receiver.htm");
