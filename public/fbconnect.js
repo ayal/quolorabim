74295109085d6da5e5e44e7c58e3416fee5922f6
Your Name <ayalgelles@gmail.com>
2011-01-19 02:18:57 +0200

    blaa

--- public/fbconnect.js
+++ public/fbconnect.js
@@ -42,40 +42,36 @@ function nav(url) {
 var fbuid = null;
 
 
+window.fbAsyncInit = function() {
+    
+    FB.init({
+		appId  : '140153199345253',
+		status : true, // check login status
+		cookie : true, // enable cookies to allow the server to access the session
+		xfbml  : true  // parse XFBML
+	    });
+    };
+
+(function() {
+     var e = document.createElement('script');
+     e.async = true;
+     e.src = document.location.protocol +
+	 '//connect.facebook.net/en_US/all.js';
+     document.getElementById('fb-root').appendChild(e);
+ }());
+
+
 //FB.init(API_KEY, siteUrl + "xd_receiver.htm");
 $(document).ready(
     function (){
 	console.log('what');
 	// initialize the library with the API key
 	
-	window.fbAsyncInit = function() {
-	   
-	    FB.init({
-			appId  : '140153199345253',
-			status : true, // check login status
-			cookie : true, // enable cookies to allow the server to access the session
-			xfbml  : true  // parse XFBML
-		    }, '/xd_receiver.htm'); 
 
-	    FB.Canvas.setSize();	    
-
-	    (function () {
-		 console.log('beforez');
-		 verifyLogin(afterLogin);
-	     })();
-
-	};
-
-	(function() {
-	     var e = document.createElement('script'); e.async = true;
-	     e.src = document.location.protocol +
-		 '//connect.facebook.net/en_US/all.js';
-	     document.getElementById('fb-root').appendChild(e);
-	 }());
-	
-
-
-
+	FB.Canvas.setSize();	    
+	console.log('beforez');
+	verifyLogin(afterLogin);
+	  
     });
 
 function evt(name, data){
