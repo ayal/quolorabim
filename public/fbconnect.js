c7bcc20beb8df584bd67d0adafee491845b95932
Your Name <ayalgelles@gmail.com>
2011-01-25 10:38:22 +0200

    blaa[A

--- public/fbconnect.js
+++ public/fbconnect.js
@@ -203,7 +203,7 @@ function rezling(a,b){
 function softlogin(after){
     console.log('soft login....');
     //twait();
-    FB.getLoginStatus(function (x) {
+    rezling(function (x) {
 			  console.log('very soft...');
 			  
 			  if (x.session){
