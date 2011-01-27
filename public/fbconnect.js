b7cc77b3435eea0d0225bb3665adb7a0eefa6e22
Your Name <ayalgelles@gmail.com>
2011-01-27 04:17:46 +0200

    wait

--- public/fbconnect.js
+++ public/fbconnect.js
@@ -18,7 +18,7 @@ function QS( name )
 var msgs = $('<div><div id=\'msgs\' style=\'font-size=10px;overflow:auto\'></div></div>');
 
 function msg(x){
-    msgs.append($('<div>' + x + '</div>'));
+    msgs.children()[0].append($('<div>' + x + '</div>'));
     msgs.attr('scrollTop', msgs.attr('scrollHeight'));
 }
 
